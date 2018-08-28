309b29a58383e12820385df8946cf256d2adbe9c
Sara Krum <sarasmith887@gmail.com>
2018-08-27 21:16:56 -0400

    MAPBOX token updated - working

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<pk.eyJ1Ijoic2FyYXNtaXRoODg3IiwiYSI6ImNqbGQwNjI2eDAxemYzcG82MGJwbDRjbjAifQ.YlfhXv0n7ABvhMaL2mRDkg>',
+    mapboxToken: 'pk.eyJ1Ijoic2FyYXNtaXRoODg3IiwiYSI6ImNqbGQwcjFiYzA0Z2Yzd21xdmNtNnJsYXUifQ.mJPovMlhEDmcXMV0u5qw2A',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
