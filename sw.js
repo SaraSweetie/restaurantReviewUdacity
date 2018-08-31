b2e360dd02bbb64641b47abdb64fd2f0fdd0e0ae
Sara Krum <sarasmith887@gmail.com>
2018-08-31 15:01:22 -0400

    service worker files caching

--- sw.js
+++ sw.js
@@ -1,7 +1,30 @@
+const offlineCache = 'v1';
+
+const cacheFiles = [
+	'index.html',
+	'restaurant/html',
+	'css/styles.css',
+	'js/main.js',
+	'js/restaurant_info.js',
+	'js/dbhelper.js',
+	'data/restaurants.json'
+];
 
 //Install
 self.addEventListener('install', event => {
 	console.log('Service worker is installed');
+
+	//Cache files in cacheFiles array
+	event.waitUntil(
+		caches.open(offlineCache)
+			.then(cache => {
+				console.log('service worker is cashing files');
+				cashe.addAll(cacheFiles);
+			})
+			.then( ()=> {
+				self.skipWaiting()
+			})
+	);
 })
 
 //Activate
